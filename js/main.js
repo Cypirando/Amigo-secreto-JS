a48e44ecf6a9a6f16880636198bea73e3b1d4284
cypirando <fabiocypriano@gmail.com>
2022-12-12 12:00:46 -0300

    tete

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ let printNomeDoSorteio = document.getElementById('nome-sorteio')
 const btnAncancar = document.getElementById('btn-avancar')
 
 btnAncancar.addEventListener('click', ()=> {
+    printNomeDoSorteio = ''
     printNomeDoSorteio = nomeDoSorteio.value
-    alert(nomeDoSorteio)
+    alert(nomeDoSorteio.value)
     
 }
     
