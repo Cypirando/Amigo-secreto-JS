615ad1b468d491fcb03ee913762e22dc9a7ff60a
cypirando <fabiocypriano@gmail.com>
2022-12-12 12:44:57 -0300

    Atulização do front

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 /*infos de get e post */
 
 let nomeDoSorteio = document.getElementById('input-one') 
-let printNomeDoSorteio = document.getElementById('ns')
+let printNomeDoSorteio = document.getElementById('nome-sorteio')
 let btnAncancar = document.getElementById('btn-avancar')
 
 btnAncancar.addEventListener('click', ()=> {
     printNomeDoSorteio.innerHTML = nomeDoSorteio.value
-    alert(nomeDoSorteio.value)
+    // alert(nomeDoSorteio.value)
     
 }
     
