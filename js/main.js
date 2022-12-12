d242945078d31aedee0a51a45e0857416b5fbd46
cypirando <fabiocypriano@gmail.com>
2022-12-12 11:49:49 -0300

    atualização um

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-const nameSort = document.getElementById('input-one').value 
 
+/*infos de get e post */
+
+const nomeDoSorteio = document.getElementById('input-one') 
+let printNomeDoSorteio = document.getElementById('nome-sorteio')
+const btnAncancar = document.getElementById('btn-avancar')
+
+btnAncancar.addEventListener('click', ()=> {
+    printNomeDoSorteio = nomeDoSorteio.value
+    alert(nomeDoSorteio)
+    
+}
+    
+)
