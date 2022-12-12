ba41b776d98fc8ed06c030c35add8c6c411f55bd
cypirando <fabiocypriano@gmail.com>
2022-12-12 12:03:55 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -6,13 +6,11 @@ let printNomeDoSorteio = document.getElementById('ns')
 let btnAncancar = document.getElementById('btn-avancar')
 
 btnAncancar.addEventListener('click', ()=> {
-<<<<<<< HEAD
+
     printNomeDoSorteio = ''
-    printNomeDoSorteio = nomeDoSorteio.value
-=======
+
     printNomeDoSorteio.innerHTML = nomeDoSorteio.value
->>>>>>> 253419e65930589b8989f2296c52f207a652ce58
-    alert(nomeDoSorteio.value)
+
     
 }
     
