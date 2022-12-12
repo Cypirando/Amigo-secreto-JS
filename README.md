1cab6651c123cb2919f44c2092feedb8057db9fe
MauricioDiias <mauriciod664@gmail.com>
2022-12-12 15:40:36 -0300

    Conclusão da função sorteio e do html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Amigo-secreto-JS
 Pagina web que faz sorteio de nomes para amigo secreto 
+Mauricio Dias
+Fabio Cypriano
--- js/main.js
+++ js/main.js
@@ -4,14 +4,83 @@ document.getElementById("final").style.display = "grid";
 
 /*infos de get e post */
 
-let nomeDoSorteio = document.getElementById('input-one') 
+let nomeDoSorteio = document.getElementById('input-one')
 let printNomeDoSorteio = document.getElementById('nome-sorteio')
 let btnAncancar = document.getElementById('btn-avancar')
 
-btnAncancar.addEventListener('click', ()=> {
+//Nome do Sorteio
+btnAncancar.addEventListener('click', () => {
     printNomeDoSorteio.innerHTML = nomeDoSorteio.value
-    // alert(nomeDoSorteio.value)
-    
+    limparInputOne()
+})
+
+//Sorteio
+let btnAdicionar = document.querySelector('#adicionar')
+let btnSortear = document.querySelector('#btn-sortear')
+
+let participantes = []
+let cadastrados = []
+btnAdicionar.addEventListener('click', () => {
+    let input = document.querySelector('#input-dados').value
+    if (input !== '') {
+        participantes.push(input)
+        cadastrados.push({ name: input, id: cadastrados.length })
+        renderizar()
+        limparInput()
+        console.log(participantes)
+        console.log('cadastrados', cadastrados)
+    } else {
+        alert('[ERRO] Insira o Nome dos Participantes')
+    }
+})
+function limparInputOne() {
+    let input = document.getElementById("input-one");
+    input.value = "";
 }
-    
-)
+function limparInput() {
+    let input = document.getElementById("input-dados");
+    input.value = "";
+}
+
+//Render
+let quadroLista = document.querySelector('#quadro-lista')
+function renderizar() {
+    quadroLista.innerHTML = ''
+    quadroLista.innerHTML += Object.values(cadastrados).map(e =>
+        `${e.name} <br /><br />`).join('')
+}
+
+// Embaralhar
+function embaralhar(arr) {
+    // Loop em todos os elementos
+    for (let i = arr.length - 1; i > 0; i--) {
+        // Escolhendo elemento aleatório
+        const j = Math.floor(Math.random() * (i + 1));
+        // Reposicionando elemento
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    // Retornando array com aleatoriedade
+    return arr;
+}
+btnSortear.addEventListener('click', () => {
+
+    if (participantes.length >= 2) {
+        renderSorteio()
+    } else if (participantes.length == 1) {
+        alert('[ERRO] Não dar para jogar sozinho')
+    } else {
+        alert("[ERRO] Insira os Participantes")
+    }
+})
+
+function renderSorteio() {
+    let listaOne = document.getElementById('lista-one')
+    embaralhar(participantes)
+    let sorteados = [];
+    for (let i = 0; i < participantes.length; i++) {
+        sorteados.push([participantes[i], participantes[(i != participantes.length - 1) ? i + 1 : 0]]);
+    }
+    console.log('sorteados', sorteados);
+    listaOne.innerHTML = ''
+    listaOne.innerHTML += sorteados.map(i => `<div class="name-2"><h3>${i.toString().replace(",", " ==> ")}</h3></div>`).join(' ')
+}
