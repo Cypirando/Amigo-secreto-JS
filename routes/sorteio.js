3542c2c91c58a5a60a35af9e2c2c304d4e86fd94
cypirando <fabiocypriano@gmail.com>
2022-12-26 12:09:29 -0300

    mysql

--- routes/sorteio.js
+++ routes/sorteio.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("../mysql").pool;
 //Visualiza nomes
 router.get("/", (req, res, next) => {
   res.status(200).send({
@@ -8,26 +9,42 @@ router.get("/", (req, res, next) => {
 });
 //Posta nomes
 router.post("/", (req, res, next) => {
-    const user = {
-        nome: req.body.nome,
-        email: req.body.email
-    }
-    res.status(201).send({
-        mensagem: "O sorteio foi criado.",
-        usuarioCriado: user
-      });
+  // const user = {
+  //   nome: req.body.nome,
+  //   email: req.body.email,
+  // };
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "ISERT INTO usuarios (nome,email) VALUES (?,?)",
+      [req.body.nome, req.body.email],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.send(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(201).send({
+          mensagem: "O sorteio foi inserido com sucesso.",
+          id_user: resultado.insertId,
+        });
+      }
+    );
+  });
 });
+
 //Altera nomes
 router.patch("/", (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Usando o PATCH dentro da rota de Sorteios.",
-      });
+  res.status(201).send({
+    mensagem: "Usando o PATCH dentro da rota de Sorteios.",
+  });
 });
 // Exclui nomes
 router.delete("/", (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Usando o DELETE dentro da rota de Sorteios.",
-      });
+  res.status(201).send({
+    mensagem: "Usando o DELETE dentro da rota de Sorteios.",
+  });
 });
 
 module.exports = router;
